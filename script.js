15589d4a2cbfb5534cea51c993302a9df942fbcb
Rifi <103291934+rificlynophile@users.noreply.github.com>
2025-11-04 02:00:58 +0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const btn = document.getElementById("toggle");
-btn.onclick = () => {
-  document.body.classList.toggle("dark");
-  btn.textContent = document.body.classList.contains("dark") ? "☀️ Light" : "🌙 Dark";
-};
